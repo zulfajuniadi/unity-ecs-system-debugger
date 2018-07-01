df8d08f097fa9767912f4c3ca89b38a0910d956c
Zulfa Juniadi <zulfajuniadi@gmail.com>
2018-07-02 02:03:10 +0800

    Add new methods to the StringUtils

--- Assets/EntitySystemDebugger/Scripts/Editor/Utils/StringUtils.cs
+++ Assets/EntitySystemDebugger/Scripts/Editor/Utils/StringUtils.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Globalization;
 using System.Text.RegularExpressions;
+using Microsoft.CSharp;
 using UnityEngine;
 
 namespace EntitySystemDebugger.Editor.Utils
@@ -69,6 +71,32 @@ namespace EntitySystemDebugger.Editor.Utils
                 }
             }
             return str;
+        }
+
+        public static string ValidClassName (string name)
+        {
+            name = StripNonAlphanumDot (name);
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider ();
+            return codeProvider.CreateValidIdentifier (name);
+        }
+
+        public static string CreateFileName (string name)
+        {
+            name = StripNonAlphanumDot (name);
+            if (!Char.IsUpper (name, 0))
+            {
+                TextInfo textInfo = new CultureInfo ("en-US", false).TextInfo;
+                name = textInfo.ToTitleCase (name);
+            }
+            var invalids = System.IO.Path.GetInvalidFileNameChars ();
+            return String.Join ("_", name.Split (invalids, StringSplitOptions.RemoveEmptyEntries)).TrimEnd ('.');
+        }
+
+        public static string StripNonAlphanumDot (string name)
+        {
+            Regex nonAlphanum = new Regex ("[^a-zA-Z0-9.]");
+            name = nonAlphanum.Replace (name, "");
+            return name;
         }
     }
 }
